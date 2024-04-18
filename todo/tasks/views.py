6f4265b566bdf8522d186bcd15a13eb0dbffd9c0
dmarkox <markodave46@gmail.com>
2024-04-18 10:21:54 +0300

    modifications to the todo list

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import Tasks
+from datetime import datetime
 # Create your views here.
 # Create view functions for Adding, Editing, Reading and Deleting. (CRUD)
 
@@ -14,27 +15,37 @@ def taskHome(request):
     return HttpResponse(template.render(context))
 
 def addTask(request):
+    message = None
     if request.method == 'POST':
         title = request.POST['title']
         content = request.POST['content']
         task = Tasks(title=title, content=content)
         task.save()
-    return HttpResponse(loader.get_template('add.html').render())
-
-def editTask(request, id):
-    return HttpResponse("EditTask")
+        message = "Task Added Successfully"
+    context = {
+        'message': message
+    }
+    return HttpResponse(loader.get_template('add.html').render(context))
 
 def readTask(request, id):
-    # /read/:id number
-    my_id = id
-    my_tasks = Tasks.objects.get(id=my_id)
+    my_tasks = Tasks.objects.get(id=id)
     context = {
        'task': my_tasks
     }
     template = loader.get_template('viewtask.html')
     return HttpResponse(template.render(context))
 
-    
-
 def deleteTask(request, id):
-    return HttpResponse("DeleteTask")
+    Tasks.objects.filter(id=id).delete()
+    return redirect('/')
+
+def editTask(request, id):
+    time_now = datetime.now()
+    my_task = Tasks.objects.get(id=id)
+    my_task_status = my_task.status
+    if my_task_status == 0:
+        # This means that the task is pending.
+        Tasks.objects.filter(id=id).update(status=1, date=time_now)
+    elif my_task_status == 1:
+        Tasks.objects.filter(id=id).update(status=0, date=time_now)
+    return redirect('/read/'+str(id))
